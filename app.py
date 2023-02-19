8d8c53709928aeae329563985a0b8e1eab40c5e4
gaunspratik1 <101491926+gaunspratik1@users.noreply.github.com>
2023-02-19 23:44:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-import pandas as pd
+
 
 app=Flask(__name__)
 model=pickle.load(open('model.pkl','rb'))
+
+
+
+app=Flask(__name__)
+model=pickle.load(open('model_classification.pkl','rb'))
+
 @app.route('/')
 def home():
     #return 'Hello World'
@@ -15,8 +21,11 @@ def predict_api():
 
     data=request.json['data']
     print(data)
+
     new_data=[list(data.values())]
     output=model.predict(new_data)[0]
+
+
     return jsonify(output)
 
 @app.route('/predict',methods=['POST'])
@@ -26,10 +35,16 @@ def predict():
     final_features = [np.array(data)]
     print(data)
     
-    output=model.predict(final_features)[0]
-    print(output)
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="Airfoil pressure is  {}".format(output))
+
+    output = round(prediction[0], 2)
+
+    if output == -1:
+        fire = 'No Fire'
+    else:
+        fire = 'Fire'
+
+    return render_template('home.html', prediction_text="Airfoil pressure is  {}".format(fire))
+
 
 
 
